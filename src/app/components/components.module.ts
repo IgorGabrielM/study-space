da626f3da3b84b3e2fe8886192b27ffdd814f94b
igorGabrielM <igorgabprofissional@gmail.com>
2024-04-23 23:26:17 -0300

    fix: finish home page ago day 23-04 and init header

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostComponent } from './post/post.component';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
   declarations: [
-    PostComponent
+    PostComponent,
+    HeaderComponent
   ],
   exports: [
-    PostComponent
+    PostComponent,
+    HeaderComponent
   ],
   imports: [
     RouterModule,
